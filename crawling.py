3461e9a7c2e0798050007d793455942e024449de
wbw625 <625114674@qq.com>
2025-06-15 21:05:45 +0800

    update

--- crawling.py
+++ crawling.py
@@ -24,7 +24,7 @@ def get_video_ids(search_name, pages):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         # 'Host': 'api.bilibili.com',
         # 'Referer': f'https://www.bilibili.com'
-        'cookie' : r"buvid4=2048BC2C-D43E-E27A-10F8-D07A2E1CE49444643-024011402-UFC7YtxT%2Bx4AuetofCOgEg%3D%3D; is-2022-channel=1; enable_web_push=DISABLE; header_theme_version=CLOSE; CURRENT_BLACKGAP=0; DedeUserID=1683525074; DedeUserID__ckMd5=002e084d4aaa1ae9; buvid3=EDD3F1BF-5CE8-35C6-722F-D7A2FDD5718185396infoc; b_nut=1736736885; _uuid=710AE8A10C-3259-CC7C-B2A5-106A2E8CF8DC802512infoc; hit-dyn-v2=1; rpdid=|(JJmY)YRu~m0J'u~Jm~)l|ll; LIVE_BUVID=AUTO4217410661369507; buvid_fp=b0394cd9ce338cde1a2637ebb827f2c7; enable_feed_channel=ENABLE; CURRENT_QUALITY=80; home_feed_column=5; fingerprint=c330d8e9ea37dd963bb3c7dce0fa84ab; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDkyODY0MDAsImlhdCI6MTc0OTAyNzE0MCwicGx0IjotMX0.C__60Mw4KR-oxmAgYtaFNiH2eKPhIC0vG494V1H5Mgk; bili_ticket_expires=1749286340; SESSDATA=07d69450%2C1764640057%2C15386%2A62CjC-O2ghoasb974P5UKmaOfRdPysBoFPdono7EDSv_G8Sk-cQVn1_qbWCZgXVY_v0RcSVjVYUlotMTI2MmdYRUhvYnRnSm1pRkNQemtlVTRycW1XTkJKc1VLOW5PSnVaQXN2VFFMdG9aaU9Wb1VYUGZha3JxX1dBYWZuZHBYcEVMaHo1dmZuM2tRIIEC; bili_jct=296a796bd3f42923db1fd6daf3539dea; sid=6omyaf5x; browser_resolution=1552-852; bsource=search_bing; b_lsid=E8B6B1E8_1974019322C; bp_t_offset_1683525074=1074992716045090816; PVID=16; CURRENT_FNVAL=4048"
+        'cookie' : r"buvid3=E3CB765A-FE67-1247-F41C-E5018C120CC929624infoc; b_nut=1749887529; _uuid=F5E1B1013-C87C-D9510-537A-6B5CC6E14610128418infoc; enable_web_push=DISABLE; enable_feed_channel=ENABLE; buvid4=C963FF0F-F4EC-1E1E-4747-E2D2E99CD2C430487-025061415-YU%2B9YZAoJVPN4WCDFW3keA%3D%3D; buvid_fp=c330d8e9ea37dd963bb3c7dce0fa84ab; SESSDATA=b031cd0b%2C1765439558%2C62b83%2A62CjC0Wpv-3G4gDXW-sUfRDC3hMOYN3skYEN8BjwwEfeZxg19Ry63r9i89xbZqClBPSVgSVmIxeG1XRFYwRTZVMGZGLVY3clQzVm1vNnBvVzk1em16dHUtR0diSkx5WW00ZTBHWmRvSVFFUGNpN3k5YkJQWGV2MEoxbUU3THllUkx2QWtkV3pwblhnIIEC; bili_jct=b1de9fec08368e3e7b24e66b19a651b1; DedeUserID=1683525074; DedeUserID__ckMd5=002e084d4aaa1ae9; theme-tip-show=SHOWED; header_theme_version=CLOSE; home_feed_column=5; browser_resolution=1552-852; rpdid=|(JJmY)YRu~m0J'u~Rm)m~JYR; hit-dyn-v2=1; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTAxNjE4NTAsImlhdCI6MTc0OTkwMjU5MCwicGx0IjotMX0.fn-YN5Fr8RIrrJ5AcsYYxnKP_dnSfhVCFwOfJw1NwrA; bili_ticket_expires=1750161790; CURRENT_QUALITY=80; sid=817imscu; CURRENT_FNVAL=4048; b_lsid=4A175957_197735E192B; PVID=5; bp_t_offset_1683525074=1078679503087075328"
     }
     for page in range(1, pages):
         print(f'正在获取第{page}页的视频ID')
@@ -44,7 +44,7 @@ def get_video_ids(search_name, pages):
         lst_add = [item['bvid'] for item in result if 'bvid' in item]
         print(f'第{page}页', lst_add)
         bvid_lst.extend(lst_add)
-        time.sleep(10)
+        time.sleep(5)
     return bvid_lst
 
 
@@ -61,7 +61,7 @@ def get_homepage_bvids(ps=10):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
         # 'Host': 'api.bilibili.com',
         # 'Referer': f'https://www.bilibili.com/'
-        'cookie' : r"buvid4=2048BC2C-D43E-E27A-10F8-D07A2E1CE49444643-024011402-UFC7YtxT%2Bx4AuetofCOgEg%3D%3D; is-2022-channel=1; enable_web_push=DISABLE; header_theme_version=CLOSE; CURRENT_BLACKGAP=0; DedeUserID=1683525074; DedeUserID__ckMd5=002e084d4aaa1ae9; buvid3=EDD3F1BF-5CE8-35C6-722F-D7A2FDD5718185396infoc; b_nut=1736736885; _uuid=710AE8A10C-3259-CC7C-B2A5-106A2E8CF8DC802512infoc; hit-dyn-v2=1; rpdid=|(JJmY)YRu~m0J'u~Jm~)l|ll; LIVE_BUVID=AUTO4217410661369507; buvid_fp=b0394cd9ce338cde1a2637ebb827f2c7; enable_feed_channel=ENABLE; CURRENT_QUALITY=80; home_feed_column=5; fingerprint=c330d8e9ea37dd963bb3c7dce0fa84ab; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDkyODY0MDAsImlhdCI6MTc0OTAyNzE0MCwicGx0IjotMX0.C__60Mw4KR-oxmAgYtaFNiH2eKPhIC0vG494V1H5Mgk; bili_ticket_expires=1749286340; SESSDATA=07d69450%2C1764640057%2C15386%2A62CjC-O2ghoasb974P5UKmaOfRdPysBoFPdono7EDSv_G8Sk-cQVn1_qbWCZgXVY_v0RcSVjVYUlotMTI2MmdYRUhvYnRnSm1pRkNQemtlVTRycW1XTkJKc1VLOW5PSnVaQXN2VFFMdG9aaU9Wb1VYUGZha3JxX1dBYWZuZHBYcEVMaHo1dmZuM2tRIIEC; bili_jct=296a796bd3f42923db1fd6daf3539dea; sid=6omyaf5x; browser_resolution=1552-852; bsource=search_bing; b_lsid=E8B6B1E8_1974019322C; bp_t_offset_1683525074=1074992716045090816; PVID=16; CURRENT_FNVAL=4048"
+        'cookie' : r"buvid3=E3CB765A-FE67-1247-F41C-E5018C120CC929624infoc; b_nut=1749887529; _uuid=F5E1B1013-C87C-D9510-537A-6B5CC6E14610128418infoc; enable_web_push=DISABLE; enable_feed_channel=ENABLE; buvid4=C963FF0F-F4EC-1E1E-4747-E2D2E99CD2C430487-025061415-YU%2B9YZAoJVPN4WCDFW3keA%3D%3D; buvid_fp=c330d8e9ea37dd963bb3c7dce0fa84ab; SESSDATA=b031cd0b%2C1765439558%2C62b83%2A62CjC0Wpv-3G4gDXW-sUfRDC3hMOYN3skYEN8BjwwEfeZxg19Ry63r9i89xbZqClBPSVgSVmIxeG1XRFYwRTZVMGZGLVY3clQzVm1vNnBvVzk1em16dHUtR0diSkx5WW00ZTBHWmRvSVFFUGNpN3k5YkJQWGV2MEoxbUU3THllUkx2QWtkV3pwblhnIIEC; bili_jct=b1de9fec08368e3e7b24e66b19a651b1; DedeUserID=1683525074; DedeUserID__ckMd5=002e084d4aaa1ae9; theme-tip-show=SHOWED; header_theme_version=CLOSE; home_feed_column=5; browser_resolution=1552-852; rpdid=|(JJmY)YRu~m0J'u~Rm)m~JYR; hit-dyn-v2=1; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTAxNjE4NTAsImlhdCI6MTc0OTkwMjU5MCwicGx0IjotMX0.fn-YN5Fr8RIrrJ5AcsYYxnKP_dnSfhVCFwOfJw1NwrA; bili_ticket_expires=1750161790; CURRENT_QUALITY=80; sid=817imscu; CURRENT_FNVAL=4048; b_lsid=4A175957_197735E192B; PVID=5; bp_t_offset_1683525074=1078679503087075328"
     }
     resp = requests.get(url, headers=headers)
     if resp.status_code != 200:
@@ -87,7 +87,7 @@ def get_popular_video_ids(num_pages=5):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
         # 'Host': 'api.bilibili.com',
         # 'Referer': f'https://www.bilibili.com/'
-        'cookie' : r"buvid4=2048BC2C-D43E-E27A-10F8-D07A2E1CE49444643-024011402-UFC7YtxT%2Bx4AuetofCOgEg%3D%3D; is-2022-channel=1; enable_web_push=DISABLE; header_theme_version=CLOSE; CURRENT_BLACKGAP=0; DedeUserID=1683525074; DedeUserID__ckMd5=002e084d4aaa1ae9; buvid3=EDD3F1BF-5CE8-35C6-722F-D7A2FDD5718185396infoc; b_nut=1736736885; _uuid=710AE8A10C-3259-CC7C-B2A5-106A2E8CF8DC802512infoc; hit-dyn-v2=1; rpdid=|(JJmY)YRu~m0J'u~Jm~)l|ll; LIVE_BUVID=AUTO4217410661369507; buvid_fp=b0394cd9ce338cde1a2637ebb827f2c7; enable_feed_channel=ENABLE; CURRENT_QUALITY=80; fingerprint=c330d8e9ea37dd963bb3c7dce0fa84ab; home_feed_column=5; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDk4MDQ3OTMsImlhdCI6MTc0OTU0NTUzMywicGx0IjotMX0.ojtbN-Q1m8NDsJqTcLkvyoYELL4MDA7B48fEnibaKJs; bili_ticket_expires=1749804733; browser_resolution=1552-852; PVID=2; b_lsid=61028EA77_197641F6789; SESSDATA=c167da32%2C1765283709%2C329d6%2A62CjCZ-6JqGUjgnIqK41z1zWSmVkNOnGdWrXOOsqqYw-Usdr9s3dkKoGB3lQK9MrCXXYASVklOR1JOQXpid0NsQU9ERTBBR1FnMVhRTnhYQnl3MHVySU1YUkpBa0VyWk80OFVBYkQyNWVBS1VNLUFqR0xmVExBWTJBTEw2Wm5MbEw1Sk1JT0VwMWt3IIEC; bili_jct=bbd9c70b16405af7577311846299ee79; sid=5dfe9mk9; bsource=search_bing; CURRENT_FNVAL=2000; bp_t_offset_1683525074=1077592180576485376"
+        'cookie' : r"buvid3=E3CB765A-FE67-1247-F41C-E5018C120CC929624infoc; b_nut=1749887529; _uuid=F5E1B1013-C87C-D9510-537A-6B5CC6E14610128418infoc; enable_web_push=DISABLE; enable_feed_channel=ENABLE; buvid4=C963FF0F-F4EC-1E1E-4747-E2D2E99CD2C430487-025061415-YU%2B9YZAoJVPN4WCDFW3keA%3D%3D; buvid_fp=c330d8e9ea37dd963bb3c7dce0fa84ab; SESSDATA=b031cd0b%2C1765439558%2C62b83%2A62CjC0Wpv-3G4gDXW-sUfRDC3hMOYN3skYEN8BjwwEfeZxg19Ry63r9i89xbZqClBPSVgSVmIxeG1XRFYwRTZVMGZGLVY3clQzVm1vNnBvVzk1em16dHUtR0diSkx5WW00ZTBHWmRvSVFFUGNpN3k5YkJQWGV2MEoxbUU3THllUkx2QWtkV3pwblhnIIEC; bili_jct=b1de9fec08368e3e7b24e66b19a651b1; DedeUserID=1683525074; DedeUserID__ckMd5=002e084d4aaa1ae9; theme-tip-show=SHOWED; header_theme_version=CLOSE; home_feed_column=5; browser_resolution=1552-852; rpdid=|(JJmY)YRu~m0J'u~Rm)m~JYR; hit-dyn-v2=1; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTAxNjE4NTAsImlhdCI6MTc0OTkwMjU5MCwicGx0IjotMX0.fn-YN5Fr8RIrrJ5AcsYYxnKP_dnSfhVCFwOfJw1NwrA; bili_ticket_expires=1750161790; CURRENT_QUALITY=80; sid=817imscu; CURRENT_FNVAL=4048; b_lsid=4A175957_197735E192B; PVID=5; bp_t_offset_1683525074=1078679503087075328"
     }
     video_ids = []
     start_page = 300
@@ -116,26 +116,33 @@ def main():
     # Search for videos by name
     # search_name = '大东彦'
     # prefix = '【大东彦】'
-    # pages = 20  # Number of pages to scrape
+    # pages = 32  # Number of pages to scrape
     # video_ids = get_video_ids(search_name, pages)
 
     # Recommended videos from Bilibili homepage
-    # num_pages = 5
-    # search_name = 'B站首页推荐'
-    # num_bvids = 10
-    # video_ids = []
-    # for _ in range(num_pages):
-    #     print(f"Fetching video IDs from page {_ + 1} of {num_pages}...")
-    #     video_ids.extend(get_homepage_bvids(ps=num_bvids))
-    #     time.sleep(10)
+    num_pages = 60
+    search_name = 'B站首页推荐'
+    prefix = ''
+    num_bvids = 10
+    video_ids = []
+    for _ in range(num_pages):
+        print(f"Fetching video IDs from page {_ + 1} of {num_pages}...")
+        # 如果重复，跳过
+        new_video_ids = get_homepage_bvids(ps=num_bvids)
+        for video_id in new_video_ids:
+            if video_id not in video_ids:
+                video_ids.append(video_id)
+                print(f"Added new video ID: {video_id}")
+        print(f"Fetched {len(new_video_ids)} new video IDs.")
+        time.sleep(5)
 
     # Popular videos from Bilibili homepage
-    search_name = 'B站热门视频'
-    prefix = ''
-    num_pages = 5
-    video_ids = []
-    video_ids.extend(get_popular_video_ids(num_pages))
-    time.sleep(10)
+    # search_name = 'B站每周必看'
+    # prefix = ''
+    # num_pages = 20
+    # video_ids = []
+    # video_ids.extend(get_popular_video_ids(num_pages))
+    # time.sleep(5)
 
 
     print(f"Found {len(video_ids)} videos to scrape.")
@@ -143,10 +150,13 @@ def main():
 
     for video_id in video_ids:
         try:
-            time.sleep(1)
+            time.sleep(0.5)
             video_data = scrape_bilibili_video(video_id, prefix)
             if video_data is None:
                 print(f"Video {video_id} does not match criteria, skipping.")
+                continue
+            if video_data in all_videos_data:
+                print(f"Video {video_id} is a duplicate, skipping.")
                 continue
             all_videos_data.append(video_data)
         except Exception as e:
